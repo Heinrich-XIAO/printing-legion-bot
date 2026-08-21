487888be54dce0b1fb0fe347f175317874c40eec
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-08-21 10:31:28 -0400

    init project ig

---
+++ index.ts
@@ -0,0 +1,16 @@
+import { App } from "@slack/bolt";
+import { config } from "dotenv";
+
+config();
+
+const app = new App({
+  token: process.env.BOT_TOK,
+  appToken: process.env.APP_TOK,
+  socketMode: true,
+  signingSecret: process.env.SIGNING_SECRET,
+});
+
+(async () => {
+  await app.start();
+  console.log("functionable :skulk:");
+})();
