f01f1ca9ea950d993e26cd6b798767c5d226128e
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2026-08-24 21:47:26 -0400

    ignore threads and botids

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ app.command("/update-filament-stock", async ({ command, ack, respond }) => {
 
 app.event("message", async ({ event }) => {
   console.log("LOG: message event", event);
-  if (event.type == "message" && event.subtype == undefined) {
+  if (event.type == "message" && event.subtype == undefined && !event.thread_ts && !event.bot_id) {
     const startms = process.hrtime.bigint()/1000000n;
     const response = await parseInfo(event.text);    
     const endms = process.hrtime.bigint()/1000000n;
